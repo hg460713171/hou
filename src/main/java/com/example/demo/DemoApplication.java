3ab35760c6f52e033209955512b015b3e001fdcc
houjiahao <houjiahao@DESKTOP-DDT1CDA>
2019-09-22 17:26:14 +0800

    添加数据源

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,7 +2,8 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//排除数据源
+//@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @SpringBootApplication
 public class DemoApplication {
 
